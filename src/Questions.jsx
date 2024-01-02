7c82060b60577a42a71ecc48c499b308e2e0de10
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2024-01-02 14:44:21 +0530

    Initialised CRA and Tailwind

---
+++ src/Questions.jsx
@@ -0,0 +1,51 @@
+const Questions = [
+    {
+      text: "What is ReactJS?",
+      options: [
+        { id: 0, text: "Server-side framework", isCorrect: false },
+        { id: 1, text: "user interface framework", isCorrect: true },
+        { id: 2, text: "both a and b", isCorrect: false },
+        { id: 3, text: "none of the above", isCorrect: false },
+      ],
+    },
+  
+    {
+      text: "React.js is written in which of the following language?",
+      options: [
+        { id: 0, text: "JavaScript", isCorrect: "fill a boolean value here like shown in the first question" },
+        { id: 1, text: "Java", isCorrect:  "fill a boolean value here like shown in the first question" },
+        { id: 2, text: "C", isCorrect:  "fill a boolean value here like shown in the first question" },
+        { id: 3, text: "C++", isCorrect:  "fill a boolean value here like shown in the first question" },
+      ],
+    },
+    {
+      text: "What is a state in React?",
+      options: [
+        { id: 0, text: "A permanent storage.", isCorrect:  "fill a boolean value here like shown in the first question" },
+        { id: 1, text: "Internal storage of the component.", isCorrect: "fill a boolean value here like shown in the first question" },
+        { id: 2, text: "External storage of the component.", isCorrect:  "fill a boolean value here like shown in the first question" },
+        { id: 3, text: "None of the above.", isCorrect:  "fill a boolean value here like shown in the first question" },
+      ],
+    },
+    
+    {
+      text: "What is the return value of the useState hook?",
+      options: [
+        { id: 0, text: " Pair of current state and function updating it", isCorrect: "fill a boolean value here like shown in the first question" },
+        { id: 1, text: "Current State", isCorrect:  "fill a boolean value here like shown in the first question" },
+        { id: 2, text: "Function updating the current state", isCorrect:  "fill a boolean value here like shown in the first question" },
+        { id: 3, text: "UseState returns nothing", isCorrect:  "fill a boolean value here like shown in the first question" },
+      ],
+    },
+    {
+      text: "How many elements can a valid react component return?",
+      options: [
+        { id: 0, text: "1", isCorrect: "fill a boolean value here like shown in the first question" },
+        { id: 1, text: "2", isCorrect:  "fill a boolean value here like shown in the first question" },
+        { id: 2, text: "3", isCorrect:  "fill a boolean value here like shown in the first question" },
+        { id: 3, text: "4", isCorrect:  "fill a boolean value here like shown in the first question" },
+      ],
+    },
+  ];
+  
+  export default Questions;
---
+++ src/components/QuestionBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function QuestionBox() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
